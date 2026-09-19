771525cb6e02edb886ad8be50fb19e1c1489302b
Abdelrahman Adam <abdelrahmanadam53@gmail.com>
2026-09-19 14:55:00 +0300

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,10 +1,10 @@
-// =========================
-// MOBILE NAVBAR
-// =========================
-
 const menuToggle = document.getElementById("menuToggle");
 const navMenu = document.querySelector(".nav-menu");
 
-menuToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
-});
+if (menuToggle && navMenu) {
+
+    menuToggle.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+    });
+
+}
